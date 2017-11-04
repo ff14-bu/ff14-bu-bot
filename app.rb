e71751b355bf642d0454d8947c5d5b8627a69422
Kuniaki Hori <holygrail81@gmail.com>
2017-11-04 10:48:12 +0900

    use CommandBot

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require 'discordrb'
 token = ENV["DISCORD_BOT_TOKEN"]
 client_id = ENV["DISCORD_CLIENT_ID"]
 
-bot = Discordrb::Bot.new token: token, client_id: client_id
+bot = Discordrb::Commands::CommandBot.new token: token, client_id: client_id, prefix: '/'
 
 bot.mention(contains: 'ping') do |event|
   event.respond 'pong'
+end
+
+bot.command :eriones do |_event, *args|
+  "#{args[0]}をエリオネスで検索します"
 end
 
 bot.run
